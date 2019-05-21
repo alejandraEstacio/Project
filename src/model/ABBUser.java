9b2550d185310ff6487545ff16a5b08db64826c3
MARIA ALEJANDRA <MARIA ALEJANDRA@172.30.176.201>
2019-05-21 16:07:35 -0500

    proyecto

---
+++ src/model/ABBUser.java
@@ -0,0 +1,18 @@
+package model;
+
+public class ABBUser {
+	
+	private User root;
+	public ABBUser() {
+		root = null;
+	}
+	public User getRoot() {
+		return root;
+	}
+	public void setRoot(User root) {
+		this.root = root;
+	}
+	
+	
+
+}
